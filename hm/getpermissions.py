75082cf0a1675caee586410697dbc0264bb14b99
fanshaohua <fanshaohua@huimei.com>
2021-03-10 15:26:16 +0800

    修改权限代码

--- hm/getpermissions.py
+++ hm/getpermissions.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 import os
 
 import requests
@@ -6,10 +7,13 @@ import json
 
 def get_customer_id():
 
-    # url = "http://hmservice/"
+    #测试环境
     # url = "http://test-mayson.huimeionline.com/cdss/sentry/customerAuth/getCustomerId"
+
+    #预发环境
     # url = "http://profile.huimeionline.com/cdss/sentry/customerAuth/getCustomerId"
 
+    #医院现场
     url = "http://hmservice/cdss/sentry/customerAuth/getCustomerId"
 
     headers = {
@@ -29,9 +33,13 @@ def get_customer_id():
 
 def get_permission():
 
+    #测试环境
     # url = "http://test-mayson.huimeionline.com/cdss/sentry/customerAuth/getProductNames"
+
+    #预发环境
     # url = "http://profile.huimeionline.com/cdss/sentry/customerAuth/getProductNames"
 
+    #医院现场
     url = "http://hmservice/cdss/sentry/customerAuth/getProductNames"
 
     headers = {
@@ -46,6 +54,7 @@ def get_permission():
         print(url)
 
     permission_dict = json.loads(response.content.decode('utf-8'))
+    # permission_dict = {'customizedProduct': ['None','None','Mayson问诊系统患者版', '文献', 'AME', '数据上报', '单病种数据上报', '肿瘤化疗质控', '独立知识库', 'DRG', 'CDR'], 'hospitalization': ['智能推荐mayson住院版', '治疗方案住院版', '知识库住院版'], 'outpatient': ['cdss门诊版', '知识库门诊版', '慢病用药门诊版', '用药合理性门诊版', '治疗方案门诊版', '问诊系统门诊版'], 'qualityControl': ['成人社区获得性肺炎', '小儿肺炎', '川崎病', '非ST段抬高型心肌梗死', '急性心肌梗死', '脑梗死', '短暂性脑缺血发作', '主动脉夹层', '心力衰竭', '妊娠期高血压疾病', '扩张型心肌病', '心绞痛', '肥厚性心肌病', '房颤', '癫痫', '支气管哮喘', '慢性阻塞性肺疾病急性加重', '急性肺栓塞', '肺动脉高压', '永久性心脏起搏器安置术', '经皮冠状动脉介入治疗', '心脏瓣膜置换术', '乳腺癌', '急性冠脉综合征', 'TIA', 'CABG手术', '髋关节置换术', '膝关节置换术', '剖宫产手术', '泌尿道感染', '紫癜性肾炎', '先天性心脏病', 'PCI手术', '脑出血', '疑似感染', '冠状动脉搭桥术', '测试病种', '围手术期预防感染', '肿瘤化疗', '法洛氏四联症根治术', '射频消融术', '主动脉瓣置换术', '二尖瓣置换术', '房间隔缺损手术', '室间隔缺损手术', '支气管炎', '肺癌', '甲状腺癌', '胃癌', '帕金森病', '急性动脉瘤性蛛网膜下腔出血', '胶质瘤', '住院精神疾病', '脑膜瘤', '垂体腺瘤', '异位妊娠', '子宫肌瘤', '甲状腺结节'], 'recordsQualityControl': ['病历质控', '病案质控'], 'riskWarning': {'AI': 1, 'diseaseType': ['孕产妇静脉血栓栓塞', '脓毒症', '糖尿病黄斑水肿管理', '静脉血栓栓塞症', 'APACHE II评分', '房颤管理', 'None','深静脉血栓形成', '肺血栓栓塞症', '血脂异常', '复旦肿瘤妇科VTE']}}
     print(permission_dict)
     return permission_dict
 
@@ -60,7 +69,11 @@ def write_command(permission_dict):
 
     #门诊版
     if "outpatient" in permission_dict.keys():
-        for item in permission_dict["outpatient"]:
+        outpatient_list = list(filter(None, permission_dict["outpatient"]))
+        for item in outpatient_list:
+
+            if item == 'None':
+                continue
 
             customized_product_str = customized_product_str + "门诊版/" + item + " "
 
@@ -68,23 +81,35 @@ def write_command(permission_dict):
     if "hospitalization" in permission_dict.keys():
         for item in permission_dict["hospitalization"]:
 
+            if item == 'None':
+                continue
+
             customized_product_str = customized_product_str + "住院版/" + item + " "
 
     #病历质控
     if "recordsQualityControl" in permission_dict.keys():
         for item in permission_dict["recordsQualityControl"]:
 
+            if item == 'None':
+                continue
+
             customized_product_str = customized_product_str + "病历质控/" + item + " "
 
     #其他的一坨
-    if "product" in permission_dict.keys():
-        for item in permission_dict["product"]:
+    if "customizedProduct" in permission_dict.keys():
+        for item in permission_dict["customizedProduct"]:
+
+            if item == 'None':
+                continue
 
             customized_product_str = customized_product_str + item + " "
 
     #质控
     if "qualityControl" in permission_dict.keys():
         for item in permission_dict["qualityControl"]:
+
+            if item == 'None':
+                continue
 
             quality_control_str = quality_control_str + "质控/" + item + " "
 
@@ -93,6 +118,8 @@ def write_command(permission_dict):
         if permission_dict["riskWarning"]["AI"] == 0:
 
             for item in permission_dict["riskWarning"]["diseaseType"]:
+                if item == 'None':
+                    continue
                 if item == "APACHE II评分":
                     risk_warning = risk_warning + "临床风险预警/" + "APACHEII评分 "
                 else:
@@ -102,6 +129,8 @@ def write_command(permission_dict):
         else:
 
             for item in permission_dict["riskWarning"]["diseaseType"]:
+                if item == 'None':
+                    continue
 
                 if item == "APACHE II评分":
                     risk_warning_ai = risk_warning_ai + "临床风险预警/AI/" + "AI" + "APACHEII评分 "
